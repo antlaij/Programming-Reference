ad0d3f584ab0d59a5ce3b82bfbe84fcd1dbc0662
antlaij <antlaij@hotmail.com>
2019-08-02 10:23:03 -0400

    Update rxjs-combination.js

--- javascript/rxjs/rxjs-combination.js
+++ javascript/rxjs/rxjs-combination.js
@@ -4,15 +4,24 @@ const { map, take, concatAll, catchError, groupBy, concatMap } = require('rxjs/o
 
 const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const alphabets$ = interval(200).pipe(map(x => alphabets[x]),take(13));
+const alphabetsUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+const alphabets$ = interval(20).pipe(map(x => alphabets[x]),take(13));
 /*
 Marble Diagram:
 |-a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s-t-u-v-w-x-y-z|
 */
-const numbers$ = interval(300).pipe(map(x => numbers[x]),take(10));
+
+const numbers$ = interval(30).pipe(map(x => numbers[x]),take(10));
 /*
 Marble Diagram:
 |--0--1--2--3--4--5--6--7--8--9|
+*/
+
+const alphabetsUpper$ = interval(50).pipe(map(x => alphabetsUpper[x]),take(13));
+/*
+Marble Diagram:
+|----A----B----C----D----E----F----G----H----I----J----K----L----M----N----O----P----Q----R----S----T----U----V----W----X----Y----Z|
 */
 
 let concatAll$ = (observableName, inObservable$) => {
@@ -45,42 +54,57 @@ run$ next => [----- forkJoin End   -----]
  * When any observable emit a value
  * then subscriber will gets the  latest combined value in an Array [first, second]
  */
-const combineLatest$ = combineLatest(alphabets$, numbers$).pipe(catchError(error => of(error)));
+const combineLatest$ = combineLatest(alphabets$, numbers$, alphabetsUpper$).pipe(catchError(error => of(error)));
 const combineLatestTestCase$ = concatAll$('combineLatest', combineLatest$);
 /*
 Marble Diagram:
-alphabets$ => |-a-b-c-d-e-f-g-h-i-j-k-l-m|
-numbers$   => |--0--1--2--3--4--5--6--7--8--9|
-output     => |--ab-c-d-e-f------------------m|
-                 00 0 1 1 2                 9
-                    c   e
-                    1   2
+alphabets$       => |-a-b-c-d-e-f-g-h-i-j-k-l-m|
+numbers$         => |--0--1--2--3--4--5--6--7--8--9|
+alphabetsUpper$  => |----A----B----C----D----E----F----G----H----I----J----K----L----M|
+output           => |----bb-dde-f-ggh-i-jjk-llmm--m----m----m----m----m----m----m----m|
+                         01 122 2 244 4 566 6778  9    9    9    9    9    9    9    9
+                         AA AAA B BBB B DDD DEEE  E    G    H    H    J    K    L    M
+Same Time:
+                          c   e f  g  i j   l     m                       
+                          1   2 3  4  5 5   7     9                       
+                          A   B B  C  C D   D     f                       
 */
 /*
 Output
 run$ next => [----- combineLatest Start   -----]
-run$ next => a,0
-run$ next => b,0
-run$ next => c,0
-run$ next => c,1
-run$ next => d,1
-run$ next => e,1
-run$ next => e,2
-run$ next => f,2
-run$ next => g,2
-run$ next => g,3
-run$ next => h,3
-run$ next => i,3
-run$ next => i,4
-run$ next => j,4
-run$ next => k,4
-run$ next => k,5
-run$ next => l,5
-run$ next => m,5
-run$ next => m,6
-run$ next => m,7
-run$ next => m,8
-run$ next => m,9
+run$ next => b,0,A
+run$ next => b,1,A
+run$ next => c,1,A
+run$ next => d,1,A
+run$ next => d,1,B
+run$ next => d,2,B
+run$ next => e,2,B
+run$ next => e,3,B
+run$ next => f,3,B
+run$ next => f,3,C
+run$ next => g,3,C
+run$ next => g,4,C
+run$ next => h,4,C
+run$ next => h,5,C
+run$ next => i,5,C
+run$ next => i,5,D
+run$ next => i,6,D
+run$ next => j,6,D
+run$ next => k,6,D
+run$ next => k,6,E
+run$ next => k,7,E
+run$ next => l,7,E
+run$ next => l,8,E
+run$ next => m,8,E
+run$ next => m,8,F
+run$ next => m,9,F
+run$ next => m,9,G
+run$ next => m,9,H
+run$ next => m,9,I
+run$ next => m,9,J
+run$ next => m,9,K
+run$ next => m,9,L
+run$ next => m,9,M
 run$ next => [----- combineLatest End   -----]
 */
 
@@ -92,30 +116,31 @@ run$ next => [----- combineLatest End   -----]
  * then subscriber will gets the latest combined value in an Array [first, second] for the sam sequence
  * Output is always return any array of all the zip observables
  */
-const zip$ = zip(alphabets$, numbers$).pipe(catchError(error => of(error)));
+const zip$ = zip(alphabets$, numbers$, alphabetsUpper$).pipe(catchError(error => of(error)));
 const zipTestCase$ = concatAll$('zip', zip$);
 /*
 Marble Diagram:
-alphabets$ => |-a-b-c-d-e-f-g-h-i-j-k-l-m|
-numbers$   => |--0--1--2--3--4--5--6--7--8--9|
-output     => |--a--b--c--d- e--f--g--h--i--j|
-                 0  1  2  3  4  5  6  7  8  9
+alphabets$       => |-a-b-c-d-e-f-g-h-i-j-k-l-m|
+numbers$         => |--0--1--2--3--4--5--6--7--8--9|
+alphabetsUpper$  => |----A----B----C----D----E----F----G----H----I----J----K----L----M|
+output           => |----a----b----c----d----e----f----g----h----i----j|
+                         0    1    2    3    4    5    6    7    8    9
+                    |----A----B----C----D----E----F----G----H----I-   J|
 */
 /*
 Output
 run$ next => [----- zip Start   -----]
-run$ next => a,0
-run$ next => b,1
-run$ next => c,2
-run$ next => d,3
-run$ next => e,4
-run$ next => f,5
-run$ next => g,6
-run$ next => h,7
-run$ next => i,8
-run$ next => j,9
+run$ next => a,0,A
+run$ next => b,1,B
+run$ next => c,2,C
+run$ next => d,3,D
+run$ next => e,4,E
+run$ next => f,5,F
+run$ next => g,6,G
+run$ next => h,7,H
+run$ next => i,8,I
+run$ next => j,9,J
 run$ next => [----- zip End   -----]
-
 */
 
 /**
@@ -156,8 +181,6 @@ run$ next => 7
 run$ next => 8
 run$ next => 9
 run$ next => [----- concat End   -----]
-
-
 */
 
 const merge$ = merge(alphabets$, numbers$).pipe(catchError(error => of(error)));
@@ -196,8 +219,6 @@ run$ next => m
 run$ next => 8
 run$ next => 9
 run$ next => [----- merge End   -----]
-
-
 */
 
 
