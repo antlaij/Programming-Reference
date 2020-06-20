a53fd26daf09a3248f14503f94363beb5893eebc
Anthony Lai <antlaij@hotmail.com>
2020-06-20 09:57:07 -0400

    Add more testing on array.map and array.foreach

--- javascript/array.js
+++ javascript/array.js
@@ -1,10 +1,9 @@
-
 const { forkJoin, interval, of, concat, combineLatest, zip, merge, from, fromArray } = require('rxjs');
 const { map, take, concatAll, catchError, groupBy, concatMap, combineAll, endWith, mergeAll, delay } = require('rxjs/operators');
 
 const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const randomNumbers = [ 1, 0, 4, 2, 3, 5, 6, 7, 8, 9];
+const sortedNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const unSortedNumbers = [1, 0, 4, 2, 3, 5, 6, 7, 8, 9];
 const alphabetsUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 class FunctionTest {
@@ -15,33 +14,72 @@ class FunctionTest {
 
   startTest = () => {
     console.time(this.title);
-    console.log();console.log();
-    console.log(`[---------------   New Testing Start   ---------------]`);
-    console.log(`[-----   ${this.title}   -----]`);
     console.log();
-  }
-  
+    console.log();
+    console.log(`[-----   START: ${this.title}   -----]`);
+    console.log();
+  };
+
   endTest = () => {
-    console.log();console.log();
-    console.timeEnd(this.title);
-    console.log();console.log();
-    console.log(`[---------------   End: ${this.title}   ---------------]`);
     console.log();
-  }
+    console.log();
+    console.timeEnd(this.title);
+    console.log();
+    console.log();
+    console.log(`[---------------   END: ${this.title}   ---------------]`);
+    console.log();
+  };
 }
 
 /**
- * Sort()
+ * Testing Array.sort()
  * Function will mutate the original array
  */
-let sortTest = new FunctionTest('Array - sort() Test');
-sortTest.startTest();
-randomNumbers.sort();
-console.log('randomNumbers', randomNumbers);
-let sortedArray = randomNumbers;
-console.log('Assign randomNumbers to sortedArray', sortedArray);
+(() => {
+let testCase = new FunctionTest('Array - sort() Test');
+testCase.startTest();
+console.log('Before Calling sort()', unSortedNumbers);
+unSortedNumbers.sort();
+console.log('After Calling unSortedNumbers.sort()', unSortedNumbers);
+let sortedArray = unSortedNumbers;
+console.log('Assign unSortedNumbers to sortedArray -> Print: (sortedArray)', sortedArray);
 sortedArray.push(1000);
-console.log('randomNumbers has also changed - (randomNumbers)', randomNumbers);
-console.log('Assign randomNumbers to sortedArray - (sortedArray)', sortedArray);
-sortTest.endTest();
+console.log('Execute: sortedArray.push(1000);');
+console.log('unSortedNumbers has also changed -> Print: (unSortedNumbers)', sortedArray);
+console.log('sortedArray picks up the changes too - Print: (sortedArray)', sortedArray);
+testCase.endTest();
+})();
 
+/**
+ * Testing Array.forEach()
+ * Function will mutate the original array
+ */
+(() => {
+  let testCase = new FunctionTest('Array - forEach() Test');
+  testCase.startTest();
+  console.log('Start -> Print: (sortedNumbers)', sortedNumbers);
+  let mutatedArray = sortedNumbers.forEach((n) => {
+    n = 0;
+    return n;
+  });
+  console.log('Execute: sortedNumbers.forEach(n => n = 0); -> Print: (sortedNumbers)', sortedNumbers);
+  console.log('Print: (mutatedArray)', mutatedArray);
+  testCase.endTest();
+})();
+
+/**
+ * Testing Array.map()
+ * Function will mutate the original array
+ */
+(() => {
+  let testCase = new FunctionTest('Array - map() Test');
+  testCase.startTest();
+  console.log('Start -> Print: (sortedNumbers)', sortedNumbers);
+  let assignedArray = sortedNumbers.map((n) => {
+    n = 0;
+    return n;
+  });
+  console.log('Execute: sortedNumbers.map(); -> Print: (sortedNumbers)', sortedNumbers);
+  console.log('Print: (assignedArray)', assignedArray);
+  testCase.endTest();
+})();
