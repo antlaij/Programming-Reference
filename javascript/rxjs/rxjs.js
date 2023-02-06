c6efcc0b9df830dffab428e809958bee2eb09c59
Anthony Lai <anthony.lai@dataclef.com>
2023-02-06 18:49:25 -0500

    Fetch Data using rxjs

--- javascript/rxjs/rxjs.js
+++ javascript/rxjs/rxjs.js
@@ -17,3 +17,93 @@ interval(1000).pipe(
     )),
     take(5),
 ).subscribe(console.log);
+
+/**
+ * Fetch Data from api
+ */
+// Get Method
+rxjs.fetch.fromFetch('https://reqres.in/api/users')
+  .pipe(mergeMap((response) => response.json()))
+  .subscribe({
+    next: (data) => {
+      console.log('fetch data', data);
+    },
+    error: err => { console.error(err) }
+  });;
+ 
+  
+/**
+ * Post Method with json
+ */
+ rxjs.fetch.fromFetch('https://reqres.in/api/users', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    "name": 'User 1',
+  }),
+  selector: response => response.json()
+})
+.pipe(
+  map(response => response.data),
+)
+.subscribe({
+  next: (data) => {
+    console.log('fetch data', data);
+  },
+  error: err => { console.error(err) }
+});
+ 
+  
+/**
+ * Post Method with form data in JSON format
+ */
+ rxjs.fetch.fromFetch('https://reqres.in/api/users', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    "content-type": "application/x-www-form-urlencoded",
+  },
+  body: new URLSearchParams({
+    "name": 'User 1',
+  }),
+  selector: response => response.json()
+})
+.pipe(
+  map(response => response.data),
+)
+.subscribe({
+  next: (data) => {
+    console.log('fetch data', data);
+    buildResponsiveTabletView('repertoireTabletViewContainer', tableDef, pageLanguage, data);
+  },
+  error: err => { console.error(err) }
+});
+ 
+  
+/**
+ * Post Method with form data in Query String
+ */
+ rxjs.fetch.fromFetch('https://reqres.in/api/users', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    "content-type": "application/x-www-form-urlencoded",
+  },
+  body: "name=User 1",
+  selector: response => response.json()
+})
+.pipe(
+  map(response => response.data),
+)
+.subscribe({
+  next: (data) => {
+    console.log('fetch data', data);
+    buildResponsiveTabletView('repertoireTabletViewContainer', tableDef, pageLanguage, data);
+  },
+  error: err => { console.error(err) }
+});
+
+
